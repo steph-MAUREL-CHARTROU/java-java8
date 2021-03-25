fc2861d0f01fe504ba28e00b5fee321dc50dded6
StephanieMC <StephanieMC@StephMC.home>
2021-03-25 14:06:10 +0100

    EXO 1 et 2 ok

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -36,9 +36,11 @@ public class Lambda_02_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO transformer la liste de personnes en liste de comptes
+       
+        
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
-
+        List<Account> result = map(personList, p->new Account(p,100));
+        
         assert result.size() == personList.size();
         for (Account account : result) {
             assert account.getBalance().equals(100);
